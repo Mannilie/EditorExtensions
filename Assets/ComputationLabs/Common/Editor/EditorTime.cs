58bb4b10706de0f61ed2b650b14f1cbb8a5f6e9e
Emmanuel Vaccaro <emmanuel.vaccaro@gmail.com>
2021-10-06 16:30:19 +1100

    initial commit

---
+++ Assets/ComputationLabs/Common/Editor/EditorTime.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+namespace ComputationLabs.Common
+{
+    [InitializeOnLoad]
+    public static class EditorTime
+    {
+        public static float deltaTime = 0f;
+        private static float lastTimeSinceStartup = 0f;
+        static EditorTime()
+        {
+            EditorApplication.update -= Update;
+            EditorApplication.update += Update;
+        }
+        public static void Update()
+        {
+            if (lastTimeSinceStartup == 0f)
+                lastTimeSinceStartup = (float)EditorApplication.timeSinceStartup;
+            deltaTime = (float)EditorApplication.timeSinceStartup - lastTimeSinceStartup;
+            lastTimeSinceStartup = (float)EditorApplication.timeSinceStartup;
+        }
+    }
+}
